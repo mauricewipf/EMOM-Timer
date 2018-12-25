8d53130ac9910d7098ed0697a01af26bc4b6ba21
Maurice Wipf <maurice@gmx.de>
2018-12-25 21:36:39 +0100

    Add service-worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatToolbarModule, MatCardModule, MatButtonModule } from '@angular/material';
 import { CardComponent } from './card/card.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,7 @@ import { CardComponent } from './card/card.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : [],
     MatToolbarModule,
     MatCardModule,
     MatButtonModule
